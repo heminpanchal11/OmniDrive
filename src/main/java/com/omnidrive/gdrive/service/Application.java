a44a2340275f42aa186948b9d26679888cc00b3e
Hemin Panchal <heminpanchal11@birbucket.org>
2017-04-29 17:40:38 +0530

    Modularized Code

--- src/main/java/com/omnidrive/gdrive/service/Application.java
+++ src/main/java/com/omnidrive/gdrive/service/Application.java
@@ -34,96 +34,15 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
+import com.omnidrive.gdrive.service.*;
 
 public class Application {
 
-	
-	private static final String APPLICATION_NAME = "OmniDrive";
-
-	
-	private static final java.io.File DATA_STORE_DIR = new java.io.File(System.getProperty("user.home"),
-			".credentials/drive-java-quickstart");
-
-	/** Global instance of the {@link FileDataStoreFactory}. */
-	private static FileDataStoreFactory DATA_STORE_FACTORY;
-
-	/** Global instance of the JSON factory. */
-	private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-
-	/** Global instance of the HTTP transport. */
-	private static HttpTransport HTTP_TRANSPORT;
-
-	/**
-	 * Global instance of the scopes required by this quickstart.
-	 *
-	 * If modifying these scopes, delete your previously saved credentials at
-	 * ~/.credentials/drive-java-quickstart
-	 */
-	private static final List<String> SCOPES = Arrays.asList(DriveScopes.DRIVE);
-
-	static {
-		try {
-			HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-			DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
-		} catch (Throwable t) {
-			t.printStackTrace();
-			System.exit(1);
-		}
-	}
-
-	/**
-	 * Creates an authorized Credential object.
-	 * 
-	 * @return an authorized Credential object.
-	 * @throws IOException
-	 */
-	public static Credential authorize() throws IOException {
-		// Load client secrets.
-		InputStream in = Application.class.getResourceAsStream("/client_secret.json");
-		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-		// Build flow and trigger user authorization request.
-		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY,
-				clientSecrets, SCOPES).setDataStoreFactory(DATA_STORE_FACTORY).setAccessType("offline").build();
-		Credential credential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
-		System.out.println("Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
-		return credential;
-	}
-
-	/**
-	 * Build and return an authorized Drive client service.
-	 * 
-	 * @return an authorized Drive client service
-	 * @throws IOException
-	 */
-	public static Drive getDriveService() throws IOException {
-		Credential credential = authorize();
-		return new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential).setApplicationName(APPLICATION_NAME).build();
-	}
-
 	public static void main(String[] args) throws IOException {
 
-		// Build a new authorized API client service.
-		Drive service = getDriveService();
-
-		// Print the names and IDs for up to 10 files.
-		FileList result = service.files().list().execute();
-		List<File> files = result.getItems();
-		if (files == null || files.size() == 0) {
-			System.out.println("No files found.");
-		} else {
-			System.out.println("Files:");
-			int i = 0;
-
-			int count = (int) files.parallelStream()	.count();
-			System.out.println(count);
-			/*
-			 * for (File file : files) { //System.out.println();
-			 * System.out.print(i++ +file.getId() + file.getTitle());
-			 * 
-			 * }
-			 */
-		}
+		Crud crudservice= new Crud();
+		System.out.println(crudservice.fetchFiles(10));
+		
 	}
 
 }
--- src/main/java/com/omnidrive/gdrive/service/Crud.java
+++ src/main/java/com/omnidrive/gdrive/service/Crud.java
@@ -1,5 +1,68 @@
 package com.omnidrive.gdrive.service;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
+
 public class Crud {
+
+	private static final String APPLICATION_NAME = "OmniDrive";
+	private static final java.io.File DATA_STORE_DIR = new java.io.File(System.getProperty("user.home"),
+			".credentials/drive-java-quickstart");
+	private static FileDataStoreFactory DATA_STORE_FACTORY;
+	private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+	private static HttpTransport HTTP_TRANSPORT;
+	private static final List<String> SCOPES = Arrays.asList(DriveScopes.DRIVE);
+	static {
+		try {
+			HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+			DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
+		} catch (Throwable t) {
+			t.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	public static Credential authorize() throws IOException {
+		// Load client secrets.
+		InputStream in = Crud.class.getResourceAsStream("/client_secret.json");
+		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+		// Build flow and trigger user authorization request.
+		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY,
+				clientSecrets, SCOPES).setDataStoreFactory(DATA_STORE_FACTORY).setAccessType("offline").build();
+		Credential credential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+		System.out.println("Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
+		return credential;
+	}
+
+	public static Drive getDriveService() throws IOException {
+		Credential credential = authorize();
+		return new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential).setApplicationName(APPLICATION_NAME).build();
+	}
+
+	public static List<File> fetchFiles(int limit) throws IOException {
+		// limit integer will limit number of files, instead of fetching all of them.
+		FileList result = getDriveService().files().list().setMaxResults(limit).execute();
+		List<File> files = result.getItems();
+		return files;
+	}
 
 }
